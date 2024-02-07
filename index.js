c6165ff092af7a31b0a8ee2309a98e4149ee859c
Leilani <pquaby@gmail.com>
2024-02-06 21:04:32 -0500

    added function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
+// Sum all the values from fromN up to toN
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return toN;
+  }
+  return fromN + sum(fromN + 1, toN);
+};
+
+
+console.log(sum(3, 7));
 
 module.exports = sum;
